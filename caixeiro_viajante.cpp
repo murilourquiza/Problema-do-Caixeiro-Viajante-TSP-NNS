0eee80f482cd568eb400da00c480864381e5da2d
Yago de Andrade <70113380+YagoAndrade@users.noreply.github.com>
2021-04-29 23:31:11 -0300

    Atualizando o código principal
    Corrigindo alguns bugs no tratamento de entrada.

--- caixeiro_viajante.cpp
+++ caixeiro_viajante.cpp
@@ -5,7 +5,7 @@
 #include <cstring>
 #include <cmath>
 #define MAX 100
- 
+
 using namespace std;
 
 typedef struct elemento { int dado_cidade;    struct elemento *prox; } Elem;
@@ -18,6 +18,7 @@ Lista* makeList();
 void showError(int);
 void freeList(Lista*);
 void printList(Lista*);
+void removeSpaces(char *);
 int howManyDigits(int, int);
 double calculateDistance(ponto, ponto);
 void putCoordinatesInMatrix(int, ponto *);
@@ -32,43 +33,42 @@ int main(void) {
     takeInput();
     return 0;
 }
- 
+
 void takeInput() {
     /* 1. Recebe a quantidade de cidades existentes no universo */
- 
+
     unsigned int MAX_CIDADES;
     cout << "Insira a quantidade de cidades por onde o vendedor vai passar: ";
     cin >> MAX_CIDADES;
     getchar();
  
-    // Se não existirem cidades (ou existirem cidades negativas), o vendedor não pode viajar.
+    // Se não existirem cidades (ou cidades negativas), o vendedor não pode viajar.
     if(MAX_CIDADES <= 0) {
         showError(422);
     }
  
     /* 2. Recebe as coordenadas das cidades e as armazena no array "coordenadas" */
  
-    ponto coordenadas[100];         string coordenadas_existentes;
+    ponto coordenadas[MAX];         string coordenadas_existentes;
  
     cout << "Insira as coordenadas das cidades e aperte enter." << "\n" << "> ";
     getline(std::cin, coordenadas_existentes);
  
     // Filtra os valores recebidos e coloca-os nas coordenadas
  
-    removeCharsFromString(coordenadas_existentes, const_cast<char*>("()"), coordenadas_existentes.length());
+    removeCharsFromString(coordenadas_existentes, const_cast<char*>("()!@#$&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMONQRSTUVWXYZ"), coordenadas_existentes.length());
     allocateCoordinates(coordenadas, coordenadas_existentes, coordenadas_existentes.length(), MAX_CIDADES);
  
     /* 3. Recebe a coordenada inicial do vendedor e a filtra, removendo caracteres desnecessários */
  
     ponto p_coordenada_inicial;     string s_coordenada_inicial;
- 
+
     cout << "Insira a coordenada inicial do vendedor" << "\n" << "> ";
     getline(std::cin, s_coordenada_inicial);
  
     // Filtra os valores iniciais, antes de passá-los para um ponto.
  
-    removeCharsFromString(s_coordenada_inicial, const_cast<char*>("()"), s_coordenada_inicial.length());
- 
+    removeCharsFromString(s_coordenada_inicial, const_cast<char*>("()!@#$&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMONQRSTUVWXYZ"), s_coordenada_inicial.length());
     allocateStartingCoordinates(s_coordenada_inicial, &p_coordenada_inicial, s_coordenada_inicial.length());
  
     /*  4. Verificar se a coordenada inserida como coordenada inicial pertence ao universo de coordenadas,
@@ -115,6 +115,8 @@ void allocateCoordinates(ponto *coordenadas, string &coordenadasInput, int coord
                     coordenadasInput[q] = ' ';
                 }
             }
+        } else {
+            coordenadasInput[l] == ' ';
         }
     }
 
@@ -130,15 +132,35 @@ void allocateStartingCoordinates(string &s_coordenada_inicial, ponto *p_coordena
         }
     }
  
-    p_coordenada_inicial->x = stoi(s_coordenada_inicial);
+    char char_array[lengthCoordenadaInicial + 1];
+    strcpy(char_array, s_coordenada_inicial.c_str());
+
+    removeSpaces(char_array);
+ 
+    string someString(char_array);
+
+    p_coordenada_inicial->x = stoi(someString);
     
     for(int w = 0; w < howManyDigits(p_coordenada_inicial->x, 0); w++) {
-        s_coordenada_inicial[w] = ' ';
+        someString[w] = ' ';
     }
  
-    p_coordenada_inicial->y = stoi(s_coordenada_inicial);
+    p_coordenada_inicial->y = stoi(someString);
 }
- 
+
+void removeSpaces(char *str) {
+    int contador = 0;
+  
+    int i = 0;
+    for (i = 0; str[i]; i++)
+        if (str[i] != ' ') {
+            str[contador++] = str[i];
+            str[contador++] = ' ';
+            i++;
+        }
+    str[contador] = '\0';
+}
+
 bool verifyBelonging(int MAX_CIDADES, ponto *coordenadas, ponto *p_coordenada_inicial, bool coordenada_pertence) {
     for(int p = 0; p < MAX_CIDADES; p++) {
         if((p_coordenada_inicial->x == coordenadas[p].x) && (p_coordenada_inicial->y == coordenadas[p].y)) {
@@ -162,11 +184,11 @@ void putCoordinatesInMatrix(int MAX_CIDADES, ponto *coordenadas) {
     for(int h = 0; h < MAX_CIDADES; h++) {
         for(int k = 0; k < MAX_CIDADES; k++) {
  
-            /*  Se o i for igual a k, então estamos tratando de uma relação de si para si, e como a distância será 0, não mudaremos o que
+            /*  Se o h for igual a k, então estamos tratando de uma relação de si para si, e como a distância será 0, não mudaremos o que
                 já está definido */
  
             if(h != k) {    // Se não for uma posição de relação si para si, e ainda não tiver sido percorrido, teremos que preencher a
-                if(pontosDistancia[h][k] == 0) { // a mesma distância para pontosDistancia[i][k] e [k][i]
+                if(pontosDistancia[h][k] == 0) { // a mesma distância para pontosDistancia[h][k] e [k][h]
                     pontosDistancia[h][k] = pontosDistancia[k][h] = calculateDistance(coordenadas[h], coordenadas[k]);
                 }
             }
@@ -181,7 +203,7 @@ void putCoordinatesInMatrix(int MAX_CIDADES, ponto *coordenadas) {
         printf("\n");
     }
     printf("\n");
- 
+
     double vetor_auxiliar[MAX_CIDADES - 1];
     int cidade_visitada[MAX_CIDADES] = {};
     double total;
@@ -190,7 +212,7 @@ void putCoordinatesInMatrix(int MAX_CIDADES, ponto *coordenadas) {
     printf("Caminho: \n\n");
     while(true) {
         insertAsLastElementOfList(li, i);
-        int tamanho=0, j=0, contador=0;
+        int tamanho = 0, j = 0, contador = 0;
         double vizinho_mais_proximo;
  
         // marcar as cidades já visitadas
@@ -207,7 +229,7 @@ void putCoordinatesInMatrix(int MAX_CIDADES, ponto *coordenadas) {
         } 
  
         // no caso onde ja percorreu todas as cidades e tem que voltar para a cidade inicial;
-        if(tamanho == 0){
+        if(tamanho == 0) {
             total = total + pontosDistancia[i][0];
             printList(li);
             printf("1");
@@ -218,22 +240,22 @@ void putCoordinatesInMatrix(int MAX_CIDADES, ponto *coordenadas) {
  
         // identificar o "vizinho mais proximo"
         vizinho_mais_proximo = vetor_auxiliar[0];
-        for(contador = 1; contador < tamanho; contador++){
-            if(vetor_auxiliar[contador] < vizinho_mais_proximo){
+        for(contador = 1; contador < tamanho; contador++) {
+            if(vetor_auxiliar[contador] < vizinho_mais_proximo) {
                 vizinho_mais_proximo = vetor_auxiliar[contador];
             }
         }
  
         j = 0;
         bool encerrar = false;
-        while(encerrar == false){
-            if(cidade_visitada[j] == 0){
-                if(pontosDistancia[i][j] != vizinho_mais_proximo){
+        while(encerrar == false) {
+            if(cidade_visitada[j] == 0) {
+                if(pontosDistancia[i][j] != vizinho_mais_proximo) {
                     j++;
-                } else{
+                } else {
                     encerrar = true;
                 }
-            } else{
+            } else {
                 j++;
             }
         }
@@ -249,7 +271,7 @@ void removeCharsFromString(string &str, char* charsToRemove, int lengthString) {
     }
  
     for(int h = 0; h < lengthString; h++) {
-        if(str[h] == ',') {
+        if((str[h] == ',') || (str[h] == '.')) {
             str[h] = ' ';
         }
     }
@@ -281,10 +303,10 @@ Lista* makeList(){
     return li;
 }
  
-void freeList(Lista* li){
-    if(li != NULL){
+void freeList(Lista* li) {
+    if(li != NULL) {
         Elem* no;
-        while((*li) != NULL){
+        while((*li) != NULL) {
             no = *li;
             *li = (*li) -> prox;
             free(no);
@@ -350,14 +372,14 @@ void showError(int codigoErro) {
             cout << "Entrada incompleta - coordenadas insuficientes (400)" << "\n";
             break;
         case 404:
-            cout << "Entrada inválida - A coordenada inserida nao pertence ao universo (404)" << "\n";
+            cout << "Entrada invalida - A coordenada inserida nao pertence ao universo (404)" << "\n";
             break;
         case 422:
-            cout << "Entrada inválida - numero negativo ou nulo de cidades (422)" << "\n";
+            cout << "Entrada invalida - numero negativo ou nulo de cidades (422)" << "\n";
             break;
         default:
             cout << "Erro inesperado (-1)" << "\n";
             break;
     }
-    return;
+    exit(codigoErro);
 }
